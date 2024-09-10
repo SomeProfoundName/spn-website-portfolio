59c6d0c8722cde32ae65f8d518cb48e527033b95
Collins <84730393+SomeProfoundName@users.noreply.github.com>
2024-09-10 20:41:34 +0100

    added vite config and asset routing

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        vr: resolve(__dirname, "vr/vrgame.html"),
+        blender: resolve(__dirname, "blender/blender.html"),
+        blbs: resolve(__dirname, "blbs/benign-lootbox-statements.html"),
+        pibe: resolve(__dirname, "pibe/play-it-by-ear.html"),
+        freelance: resolve(__dirname, "freelance/freelance.html"),
+        walkingwifi: resolve(__dirname, "walking-wifi/walking-wifi.html"),
+        degreeshow: resolve(__dirname, "degree-show/degree-show.html"),
+
+      },
+    },
+  },
+});
